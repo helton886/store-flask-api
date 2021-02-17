5a1554153096a7a877171324a70f241fdbc04df4
Helton Vasconcelos <heltonvasconcelos88@gmail.com>
2021-02-17 03:07:49 -0300

    feature: sqlite3 database

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, jsonify, request
-from flask_restful import Api, Resource, reqparse
-from flask_jwt import JWT, jwt_required
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
 
 from auth import authenticate, identity
+from user import UserRgister
+from item import Item, ItemList
 
 app = Flask(__name__)
 app.secret_key = 'secret_key'
@@ -11,55 +13,9 @@ api = Api(app)
 jwt = JWT(app, authenticate, identity)  #/auth
 app.config['JWT_AUTH_HEADER_PREFIX'] = 'Bearer'
 
-items = []
-
-
-class Item(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('price',
-                        type=float,
-                        required=True,
-                        help="You need to inform the price")
-    data = parser.parse_args()
-
-    @jwt_required()
-    def get(self, name):
-        item = next(filter(lambda item: item['name'] == name, items), None)
-        if item:
-            return {'item': item}
-        return {'error': 'item no found'}, 404
-
-    def post(self, name):
-        if next(filter(lambda item: item["name"] == name, items),
-                None) is not None:
-            return {'error': f'an item with {name} already exists'}, 400
-        data = Item.parser.parse_args()
-        item = {'name': name, 'price': data['price']}
-        items.append(item)
-        return item, 201
-
-    def put(self, name):
-        data = Item.parser.parse_args()
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        if item is None:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
-        else:
-            item.update(data)
-        return item
-
-    def delete(self, name):
-        global items
-        items = list(filter(lambda x: x['name'] != name, items))
-        return {'message': 'item deleted'}
-
-
-class ItemList(Resource):
-    def get(self):
-        return {'items': items}
-
-
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+api.add_resource(UserRgister, '/register')
 
-app.run(port=3333, debug=True)
+if __name__ == '__main__':
+    app.run(port=3333, debug=True)
